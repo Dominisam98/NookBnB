ada3d9d14d6ffe911e5fbf4f8b90856e5c9a3bfc
Dominique Samuels <dominiqua6@outlook.com>
2022-02-15 19:12:45 -0500

    profile button

--- react-app/src/components/profilebutton.js
+++ react-app/src/components/profilebutton.js
@@ -37,7 +37,7 @@ function ProfileButton({ user }) {
           <li>{user.username}</li>
           <li>{user.email}</li>
           <li>
-            <button onClick={logout}>Log Out</button>
+            {/* <button onClick={logout}>Log Out</button> */}
           </li>
         </ul>
       )}
